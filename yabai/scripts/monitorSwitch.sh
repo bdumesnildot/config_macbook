ba6aa580de73b81aaf92bf771283991b593a063e
bdumesnildot <benoit.du-mesnildot@jdc.fr>
2024-05-06 08:53:38 +0200

    improve switch monitors script for more the 2 monitors

--- yabai/scripts/monitorSwitch.sh
+++ yabai/scripts/monitorSwitch.sh
@@ -4,20 +4,41 @@ debug_log_file_path="/Users/bdumesnildot/.config/yabai/scripts/debug/monitorSwit
 echo "" > "$debug_log_file_path"
 echo "windowSwitch - $(date)" >> "$debug_log_file_path"
 
+space_list=$(yabai -m query --spaces)
+spaces_id_array=($(echo "$space_list" | jq -r '.[].id'))
+number_of_spaces=${#spaces_id_array[@]}
+cur_space=$(yabai -m query --spaces --space)
+cur_space_id=$(echo "$cur_space" | jq -r '.id')
+cur_space_index=$(echo "$cur_space" | jq -r '.index')
+
 cur_window=$(yabai -m query --windows --window)
+cur_window_id=$(yabai -m query --windows --window | jq '.id')
+
+# Debug logs
+echo "\nSPACES =====================" >> "$debug_log_file_path"
+echo "space_list: $space_list" >> "$debug_log_file_path"
+echo "cur_space_id: $cur_space_id" >> "$debug_log_file_path"
+echo "\nWINDOWS =====================" >> "$debug_log_file_path"
 echo "cur_window $cur_window" >> "$debug_log_file_path"
 
-cur_space=$(yabai -m query --spaces --space)
-echo "cur_space: $cur_space" >> "$debug_log_file_path"
+# loop on spaces
+function loop_space_index() {
+  start_index=$1
+  next_space_index=$start_index
+  if [ "$start_index" -eq "$number_of_spaces" ]; then
+    next_space_index=1
+  else
+    (( next_space_index = start_index + 1 ))
+  fi
+  echo "$next_space_index"
+}
 
-cur_window=$(yabai -m query --windows --window | jq '.id')
-echo "cur_window: $cur_window" >> "$debug_log_file_path"
-
-# Move the window to the previous or next display
-yabai -m window --display prev || yabai -m window --display next
+# Move the window to the next space
+next_space_index=$(loop_space_index "$cur_space_index")
+yabai -m window --display "$next_space_index"
 
 # Focus the moved window
-yabai -m window --focus "${cur_window}"
+yabai -m window --focus "$cur_window_id"
 
 current_layout=$(yabai -m query --spaces --space | jq '.type')
 echo "current_layout: $current_layout" >> "$debug_log_file_path"
